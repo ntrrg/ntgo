40874b448ed71fde461cc6b945d491d4d75e450b
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2018-07-17 19:16:26 -0400

    main: Add package's documentation

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Copyright 2018 Miguel Angel Rivera Notararigo. All rights reserved.
+// This source code was released under the MIT license.
+
+/*
+Package ntgo provides a personal standart library for ntrrg.
+*/
+package ntgo
