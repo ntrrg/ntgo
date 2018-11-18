02f67b644878dd73713f423d501a0077967eb4dd
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2018-11-17 23:37:09 -0700

    net/http/middleware: Add ReplaceURL middleware

--- net/http/middleware/url.go
+++ net/http/middleware/url.go
@@ -3,7 +3,27 @@
 
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+	"url"
+)
+
+// ReplaceURL replaces old by new from the request URL.
+func ReplaceURL(old, new string) Adapter {
+	return func(h http.Handler) http.Handler {
+		nh := func(w http.ResponseWriter, r *http.Request) {
+			r2 := new(http.Request)
+			*r2 = *r
+			r2.URL = new(url.URL)
+			*r2.URL = *r2.URL
+			r2.URL.Path = strings.Replace(r.URL.Path, old, new, 1)
+			h.ServeHTTP(w, r2)
+		}
+
+		return http.HandlerFunc(nh)
+	}
+}
 
 // StripPrefix strips the given prefix from the request URL.
 func StripPrefix(prefix string) Adapter {
