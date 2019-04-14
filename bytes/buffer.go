7d91fce865efd05359adf3d43290d69226558701
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2019-04-14 15:09:29 -0700

    bytes: Add missing BufferPool methods and tests

--- bytes/buffer.go
+++ bytes/buffer.go
@@ -12,22 +12,40 @@ type BufferPool struct {
 	max  int
 }
 
-func NewBufferPool(n, max int) *BufferPool {
+func NewBufferPool(size, max int) *BufferPool {
 	return &BufferPool{
-		pool: make(chan *bytes.Buffer, n),
+		pool: make(chan *bytes.Buffer, size),
 		max:  max,
 	}
 }
 
+func (bp *BufferPool) Add(buf *bytes.Buffer) {
+	if buf == nil || buf.Len() > bp.max {
+		buf = newBufferL(bp.max)
+	}
+
+	bp.pool <- buf
+}
+
+func (bp *BufferPool) Cap() int {
+	return cap(bp.pool)
+}
+
 func (bp *BufferPool) Clear() {
-	for range bp.pool {
+	clearing:
+	for {
+		select {
+		case <- bp.pool:
+		default:
+			break clearing
+		}
 	}
 }
 
 func (bp *BufferPool) Fill() {
-	l := len(bp.pool)
-	for i := 0; i < l; i++ {
-		bp.pool <- newBuffer(bp.max)
+	n := bp.Cap() - bp.Len()
+	for i := 0; i < n; i++ {
+		bp.Add(nil)
 	}
 }
 
@@ -35,21 +53,17 @@ func (bp *BufferPool) Get() (buf *bytes.Buffer) {
 	select {
 	case buf = <-bp.pool:
 	default:
-		buf = newBuffer(bp.max)
+		buf = newBufferL(bp.max)
 	}
 
 	return buf
 }
 
-func (bp *BufferPool) Push(buf *bytes.Buffer) {
-	if buf.Len() > bp.max {
-		buf = newBuffer(bp.max)
-	}
-
-	bp.pool <- buf
+func (bp *BufferPool) Len() int {
+	return len(bp.pool)
 }
 
-func newBuffer(l int) *bytes.Buffer {
+func newBufferL(l int) *bytes.Buffer {
 	s := make([]byte, l)
 	return bytes.NewBuffer(s)
 }
